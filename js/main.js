6a179813d8fb10c02d4feca665d6885f383b16ca
vitaliykravchyk <vetal.kravchik@gmail.com>
2019-01-31 01:40:23 +0200

    update version

--- js/main.js
+++ js/main.js
@@ -1,54 +1,95 @@
-var ToDo =  {
-    options: {
-        taskBar: document.getElementById('taskBar'),
-        content: document.getElementById('content'),
-        addItem: document.getElementById('addItem'),
-        close: document.getElementsByClassName('close'),
-        textForP: ''
-    },
-
+let ToDo =  {
+    data: [],
     init: function(){
-        var self = this;
-        this.options.addItem.addEventListener('click', function(){
-            self.options.textForP = self.options.content.value;
-            self.createItems();
-        })
-        this.options.taskBar.addEventListener('click', function(){
-            close = document.getElementsByClassName('close');
-            if(event.target.parentNode != close[0]){
-                return
+        let self = this;
+        
+        if(localStorage.length > 0){
+            for(let i = 0; i < this.getStorage('data').length; i++){
+                this.data.push(this.getStorage('data')[i]);
+                this.createItem(this.data[i].content);
+                if(this.data[i].aproved === true){
+                    document.getElementsByTagName('p')[i].previousSibling.classList.add('approveCircle');
+                    document.getElementsByTagName('p')[i].classList.add('approveP');
+                }
             }
-            self.options.taskBar.removeChild(event.target.parentNode.parentNode)
-        })
-    },
+        };
 
-    createItems:function(){
-        if(this.options.textForP === ''){
+        document.querySelector('#content').addEventListener('keydown', function(){
+            if(event.keyCode === 13){
+                self.itemCompilation();
+            }
+        }); 
+        
+        document.querySelector('#addItem').addEventListener('click', function(){
+            self.itemCompilation();
+        });
+        document.querySelector('#taskBar').addEventListener('click', function(){
+            if(event.target.classList.contains('close')){
+                self.deleteItem();
+            } else {
+                if(event.target.classList.contains('approve')){
+                    self.approveItem();
+                }
+            }
+        });
+    },
+    itemCompilation: function(){
+        if(document.querySelector('#content').value === ''){
             alert("You can't add nothing, try to input something!");
             return
         } else {
-        var item = document.createElement('div');
+            this.data.push({content: document.querySelector('#content').value, aproved: false});
+            this.createItem(document.querySelector('#content').value);
+            this.setStorage('data', this.data);
+        }
+    },
+
+    createItem: function(value){
+        
+        let item = document.createElement('div');
         item.className = "item";
-
-        var imageCrcl = document.createElement('img');
-        imageCrcl.src = "img/circle.png";
-        var circle = document.createElement('div');
-        circle.className = "circle";
-        circle.appendChild(imageCrcl);
-       
-        var p  = document.createElement('p');
-        p.innerHTML = this.options.textForP;
-
-        var imageDlt = document.createElement('img');
-        imageDlt.src = "img/delete.png";
-        var del = document.createElement('div');
+        
+        let circle = document.createElement('button');
+        circle.className = "approve";
+        
+        let p  = document.createElement('p');
+        p.innerHTML = value;
+        
+        let del = document.createElement('button');
         del.className = "close";
-        del.appendChild(imageDlt);
         
         item.appendChild(circle);
         item.appendChild(p);
         item.appendChild(del);
-        this.options.taskBar.appendChild(item);
+        document.querySelector('#taskBar').appendChild(item);
+    },
+
+    deleteItem: function(){
+        localStorage.removeItem('data');
+        this.data.splice(this.data.findIndex(function(elem){//вырезает с массива объект который ответственный за текущий item
+            if(elem.content === event.target.previousSibling.textContent){
+                return elem
+            }
+        }), 1);
+        this.setStorage('data', this.data);
+        event.target.parentNode.remove(); 
+    },
+
+    approveItem: function(){
+        localStorage.removeItem('data');
+        event.target.nextSibling.classList.toggle('approveP')
+        event.target.classList.toggle('approveCircle');
+        for(let i = 0; i < this.data.length; i++){//change value of aproved(в объекте который ответственный за текущий item)
+            if(this.data[i].content === event.target.nextSibling.textContent){ 
+                this.data[i].aproved = !this.data[i].aproved;
+            }
         }
+        this.setStorage('data', this.data);
+    },
+    setStorage: function(key, value){
+        return localStorage.setItem(key, JSON.stringify(value));
+    },
+    getStorage: function(key){
+        return JSON.parse(localStorage.getItem(key))
     }
 }
